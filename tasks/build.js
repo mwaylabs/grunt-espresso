ba44678251bdb32c858bb8fdfdbc2889c488fe8b
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2013-12-06 15:00:55 +0100

    add server target
    changed the grunt task to a mulittask and added espresso:server target

--- tasks/build.js
+++ tasks/build.js
@@ -3,20 +3,42 @@
 module.exports = function (grunt) {
 
     var eb = require('espresso/core/commands/build.js');
+    var es = require('espresso/core/commands/server.js');
     var path = require('path');
 
-    grunt.registerTask('espresso', 'Call espresso build', function () {
-      
+    /**
+     * Register a multi task so that it can be specified which command should be run.
+     * default task is build
+     * @author Kentaro Wakayama
+     * @date   2013-12-06
+     *
+     */
+    grunt.registerMultiTask('espresso', 'call espresso', function () {
         var options = this.options({pathToApp: ''});
         var pathToApp = grunt.template.process(options.pathToApp);
         var done = this.async();
-        var args = {
-            "directory": pathToApp,
-            "help": false,
-            "onFinish": function () {
-                done();
-            }
-        };
-        eb.run(args);
+
+        if (this.target === "server") {
+            //run espresso server
+            var args = {
+                "directory": pathToApp,
+                "help": false,
+                "port": options.port,
+                "onFinish": function () {
+                    done();
+                }
+            };
+            es.run(args);
+        } else{
+            //run espresso build
+            var args = {
+                "directory": pathToApp,
+                "help": false,
+                "onFinish": function () {
+                    done();
+                }
+            };
+            eb.run(args);
+        }
     });
 };
